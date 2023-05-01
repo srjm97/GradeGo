33df5d35e14014ba96e80a106d9984e10051bec9
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-01 21:39:13 +0530

    Updated home page contents

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -23,47 +23,17 @@ const Home = (props) => {
         <div className="home-container01">
           <div className="home-card">
             <h1 className="home-text HeadingOne">GradeGo</h1>
-            <h1 className="home-text01 HeadingOne">Choose the best</h1>
+            <h1 className="home-text01 HeadingOne">Empower Your Learning Journey</h1>
             <span className="home-text02 Lead">
               <span>
-                Find the story of Creative Tim&apos;s most complex design
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <span>
-                system and all the work that make this design available.
+              Effortlessly Monitor, Measure, and Maximize Your Academic Success with Our Student Progress Tracker App!
               </span>
             </span>
             <div className="home-container02">
               <div className="home-container03">
-                <PrimaryPinkButton button="contact us"></PrimaryPinkButton>
+                <a href='/contact'><PrimaryPinkButton button="contact us"></PrimaryPinkButton></a>
               </div>
               <OutlineGrayButton button="read more"></OutlineGrayButton>
-            </div>
-            <div className="home-container04">
-              <img
-                alt="image"
-                src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/logos/gray-logos/logo-pinterest.svg"
-                className="home-image"
-              />
-              <img
-                alt="image"
-                src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/logos/gray-logos/logo-netflix.svg"
-                className="home-image01"
-              />
-              <img
-                alt="image"
-                src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/logos/gray-logos/logo-coinbase.svg"
-                className="home-image02"
-              />
-              <img
-                alt="image"
-                src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/logos/gray-logos/logo-nasa.svg"
-                className="home-image03"
-              />
             </div>
           </div>
         </div>
@@ -76,19 +46,24 @@ const Home = (props) => {
       />
       <section className="home-features">
         <FeatureCard
-          text="Choose the best design system for your next product."
-          title="Components"
+          text="Real-time tracking of your grades, assignments, and performance in one place."
+          new_prop="Real-time monitoring"
           image_src="/playground_assets/cube1.svg"
         ></FeatureCard>
-        <FeatureCard image_src="/playground_assets/person1.svg"></FeatureCard>
+        <FeatureCard 
+          image_src="/playground_assets/person1.svg"
+          text="Gain valuable insights into your strengths and weaknesses."
+          new_prop="Personalised insights"
+        ></FeatureCard>
         <FeatureCard
-          text="Make your code easier to maintain using variables."
-          title="Less Code"
+          text="Collaborate with classmates and teachers through shared features like assignment tracking."
+          new_prop="Collaborative Tools"
           image_src="/playground_assets/rocket1.svg"
         ></FeatureCard>
         <FeatureCard
-          text="This design system is fully supported on any device."
-          title="Fully Responsive"
+          text="Set academic goals, create study plans, and receive reminders to stay focused and motivated towards
+          your targets."
+          new_prop="Goal Setting and Reminders"
           image_src="/playground_assets/credit%20card1.svg"
         ></FeatureCard>
       </section>
@@ -316,6 +291,7 @@ const Home = (props) => {
               <path d="M363.722 722.052l41.298-57.816-45.254-45.256-57.818 41.296c-10.722-5.994-22.204-10.774-34.266-14.192l-11.682-70.084h-64l-11.68 70.086c-12.062 3.418-23.544 8.198-34.266 14.192l-57.818-41.298-45.256 45.256 41.298 57.816c-5.994 10.72-10.774 22.206-14.192 34.266l-70.086 11.682v64l70.086 11.682c3.418 12.060 8.198 23.544 14.192 34.266l-41.298 57.816 45.254 45.256 57.818-41.296c10.722 5.994 22.204 10.774 34.266 14.192l11.682 70.084h64l11.68-70.086c12.062-3.418 23.544-8.198 34.266-14.192l57.818 41.296 45.254-45.256-41.298-57.816c5.994-10.72 10.774-22.206 14.192-34.266l70.088-11.68v-64l-70.086-11.682c-3.418-12.060-8.198-23.544-14.192-34.266zM224 864c-35.348 0-64-28.654-64-64s28.652-64 64-64 64 28.654 64 64-28.652 64-64 64zM1024 384v-64l-67.382-12.25c-1.242-8.046-2.832-15.978-4.724-23.79l57.558-37.1-24.492-59.128-66.944 14.468c-4.214-6.91-8.726-13.62-13.492-20.13l39.006-56.342-45.256-45.254-56.342 39.006c-6.512-4.766-13.22-9.276-20.13-13.494l14.468-66.944-59.128-24.494-37.1 57.558c-7.812-1.892-15.744-3.482-23.79-4.724l-12.252-67.382h-64l-12.252 67.382c-8.046 1.242-15.976 2.832-23.79 4.724l-37.098-57.558-59.128 24.492 14.468 66.944c-6.91 4.216-13.62 8.728-20.13 13.494l-56.342-39.006-45.254 45.254 39.006 56.342c-4.766 6.51-9.278 13.22-13.494 20.13l-66.944-14.468-24.492 59.128 57.558 37.1c-1.892 7.812-3.482 15.742-4.724 23.79l-67.384 12.252v64l67.382 12.25c1.242 8.046 2.832 15.978 4.724 23.79l-57.558 37.1 24.492 59.128 66.944-14.468c4.216 6.91 8.728 13.618 13.494 20.13l-39.006 56.342 45.254 45.256 56.342-39.006c6.51 4.766 13.22 9.276 20.13 13.492l-14.468 66.944 59.128 24.492 37.102-57.558c7.81 1.892 15.742 3.482 23.788 4.724l12.252 67.384h64l12.252-67.382c8.044-1.242 15.976-2.832 23.79-4.724l37.1 57.558 59.128-24.492-14.468-66.944c6.91-4.216 13.62-8.726 20.13-13.492l56.342 39.006 45.256-45.256-39.006-56.342c4.766-6.512 9.276-13.22 13.492-20.13l66.944 14.468 24.492-59.13-57.558-37.1c1.892-7.812 3.482-15.742 4.724-23.79l67.382-12.25zM672 491.2c-76.878 0-139.2-62.322-139.2-139.2s62.32-139.2 139.2-139.2 139.2 62.322 139.2 139.2c0 76.878-62.32 139.2-139.2 139.2z"></path>
             </svg>
           </div>
+          <a ></a>
           <h2 className="home-text30 HeadingTwo">
             <span className="home-text31">Beware The Components</span>
           </h2>

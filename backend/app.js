6e8db8a2a9f43cd8e34453e8c76e87e95bf66a75
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-08 11:44:16 +0530

    app.js modified

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var mongo = require('./connect/mongoose')
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 

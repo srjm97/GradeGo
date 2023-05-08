8f9b5c229cf0c4ebc3226e82c2f0cb67559f800a
RishinR <rishinrnew2020@gmail.com>
2023-05-08 23:09:43 +0530

    Implemented user validation, PS: redirection after validation needs to be done

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const db = 'mongodb+srv://StudentTracker:EBJempYE3pbx4xSQ@cluster0.i8kgzaf.mongodb.net/node_system'
+const Login = require("./models/Login");
+
+
+mongoose.connect(db,{
+    useNewurlParser: true,
+    useunifiedTopology: true,
+    family:4
+    });
+
+
 const app = express()
 const cors = require('cors')
 //middlewares
@@ -10,9 +22,27 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     console.log(req.body)
-    res.json({status:'ok'})
+    // check in db if he is a valid user if he is then redirect to dashboard
+    const {username, password} = req.body
+    console.log(username, password)
+    const userpass = await Login.findOne({ username: username }, { password: 1 });
+    console.log(userpass)
+    if(userpass){
+      if (userpass.password == password){
+        console.log('valid user')
+        res.json({status:'ok'})
+      }
+      else {
+        console.log('invalid password')
+        res.json({status:'error', error:'Invalid Password'})
+      }
+    }
+    else {
+      console.log('invalid username')
+      res.json({status:'error', error:'Invalid Username'})
+    }
 })
 app.listen(1337, ()=>{
     console.log('Server running in http://localhost:1337')

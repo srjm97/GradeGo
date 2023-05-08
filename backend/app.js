2b8d1465c479b89074835f9a12f4b83f05e0f17a
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-08 11:48:44 +0530

    app.js edited

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 var express = require("express");
+require('dotenv').config({ path: './.env' });
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require('cors');
 var mongo = require('./connect/mongoose')
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -10,6 +12,7 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));

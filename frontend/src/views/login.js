ca2bb6de330e413672d78f26a1f4b8c55ed48ba4
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-09 13:00:49 +0530

    Updated login page

--- frontend/src/views/login.js
+++ frontend/src/views/login.js
@@ -66,7 +66,7 @@ function Login() {
                 <div className={`container ${isSignUpMode ? "sign-up-mode" : ""} ${isSignUpMode2 ? "sign-up-mode2" : ""}`}>
                     <div class="signin-signup">
                         <form onSubmit={validateUser} class="sign-in-form ">
-                            <h2 class="title">STUDENT LOGIN</h2>
+                            <h2 class="title">LOGIN</h2>
                             <div class="input-field">
                                 <FontAwesomeIcon icon={faUser} className="fontawesome" />
                                 <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" pattern="[A-Za-z0-9]+" />
@@ -83,37 +83,6 @@ function Login() {
                             
                             <p class="social-text">Forgot Password ?</p>
                         </form>
-                        <form  class="sign-up-form">
-                            <h2 class="title">FACULTY LOGIN</h2>
-                            <div class="input-field">
-                                <FontAwesomeIcon icon={faUser} className='fontawesome' />
-                                <input type="text" placeholder="Username" pattern="[A-Za-z0-9]+" />
-                            </div>
-                            <div class="input-field">
-                                <FontAwesomeIcon icon={faLock} className='fontawesome' />
-                                <input type="password" placeholder="Password" />
-                            </div>
-                            <input type="submit" value="Login" class="btn" />
-                            <p class="social-text">Forgot Password ?</p>
-                        </form>
-                    </div>
-                    <div class="panels-container">
-                        <div class="panel left-panel">
-                            <div class="content">
-                                <h3>Are you a student ?</h3>
-                                <p>Click here to continue to student login</p>
-                                <button class="btn" id="sign-in-btn" onClick={handleSignInClick}>Student</button>
-                            </div>
-                            <img src='/playground_assets/student.svg' alt="" class="image left" />
-                        </div>
-                        <div class="panel right-panel">
-                            <div class="content">
-                                <h3>Are you a faculty member ?</h3>
-                                <p>Click here to continue to faculty login</p>
-                                <button class="btn" id="sign-up-btn" onClick={handleSignUpClick}>Faculty</button>
-                            </div>
-                            <img src='/playground_assets/faculty.png' alt="" class="image right" />
-                        </div>
                     </div>
                 </div>
             </div>

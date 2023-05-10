444fb260c0e0d2d48b3f6fbb767d781c8050c6a5
RishinR <rishinrnew2020@gmail.com>
2023-05-10 12:29:40 +0530

    added time table schema and staff advisor properties

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(require('./routes/login'))
 app.use(require('./routes/users'))
 app.use(require('./routes/index'))
+app.use(require('./routes/staffAdvisor'))
 
 app.listen(1337, ()=>{
     console.log('Server running in http://localhost:1337')

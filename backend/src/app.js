1c3d9b81b6b3baaf8a916c92eeb495996feac90a
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-12 10:22:17 +0530

    eslint rules are updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,37 @@
-const express = require('express')
+const express = require("express");
 const Login = require("./models/Login");
-const mongoose = require('./connect/mongoose')
+const mongoose = require("./connect/mongoose");
 
-const app = express()
-const cors = require('cors')
+const app = express();
+const cors = require("cors");
 //middlewares
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //routes
 
-app.use(require('./routes/login'))
-app.use(require('./routes/users'))
-app.use(require('./routes/index'))
-app.use(require('./routes/staffAdvisor'))
+app.use(require("./routes/login"));
+app.use(require("./routes/users"));
+app.use(require("./routes/index"));
+app.use(require("./routes/staffAdvisor"));
+const auth = (req, res, next) => {
+  console.log(req.body);
+  if (req.body.logged) {
+    next();
+    return;
+  }
+  res.send({
+    success: false,
+    message: "Unauthorized Access",
+  });
+};
+app.post("/dashboard", auth, (req, res) => {
+  res.send({
+    success: true,
+    message: "Successfully Authenticated",
+  });
+});
 
-app.listen(1337, ()=>{
-  console.log('Server running in http://localhost:1337')
-
-})
+app.listen(1337, () => {
+  console.log("Server running in http://localhost:1337");
+});

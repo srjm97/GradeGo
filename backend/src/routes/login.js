73430b981a1aebc0ffa15bc085366b9fe5fe77f3
RishinR <rishinrnew2020@gmail.com>
2023-05-12 10:33:18 +0530

    removed warnings

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,11 +1,11 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const Login = require("../models/Login");
-const Student = require("../models/Student");
-const Faculty = require("../models/Faculty");
-const bcrypt = require("bcrypt");
+const Login = require('../models/Login');
+const Student = require('../models/Student');
+const Faculty = require('../models/Faculty');
+const bcrypt = require('bcrypt');
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   console.log(req.body);
   // check in db if he is a valid user if he is then redirect to dashboard
   const { ktu_id, password } = req.body;
@@ -23,20 +23,20 @@ router.post("/login", async (req, res) => {
       const isStudent = await Student.findOne({ _id: ktu_id });
       console.log(isStudent)
       if (isStudent) {
-        return res.json({ status: "ok", user: "student" });
+        return res.json({ status: 'ok', user: 'student' });
       } else {
         const isFaculty = Faculty.findOne({ id: ktu_id });
         if (isFaculty) {
-          return res.json({ status: "ok", user: "faculty" });
+          return res.json({ status: 'ok', user: 'faculty' });
         }
       }
     } else {
-      console.log("invalid password");
-      return res.json({ status: "error", user: 'invalid'});
+      console.log('invalid password');
+      return res.json({ status: 'error', user: 'invalid'});
     }
   } else {
-    console.log("invalid username");
-    return res.json({ status: "error", user: 'invalid'});
+    console.log('invalid username');
+    return res.json({ status: 'error', user: 'invalid'});
   }
 });
 
--- backend/src/routes/staffAdvisor.js
+++ backend/src/routes/staffAdvisor.js
@@ -1,8 +1,8 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const TimeTable = require("../models/TimeTable");
+const TimeTable = require('../models/TimeTable');
 
-router.post("/faculty", async (req, res) => {
+router.post('/faculty', async (req, res) => {
   const { _id, periods } = req.body;
 });
 

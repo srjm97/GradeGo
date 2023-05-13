01e0fcb25eae059d956667c0b2377bb8a8090dde
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-14 00:34:59 +0530

    Minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './style.css'
 import Profile from './views/contact'
 import Home from './views/home'
-import Login from './views/login'
-import Dashboard from './views/dashboard'
-import Attendance from './views/attendance'
 import FacDashboard from './views/facdashboard'
-import TimeTable from './views/timetable'
 
 const App = () => {
   return (
@@ -17,11 +13,7 @@ const App = () => {
       <div>
         <Route component={Profile} exact path="/contact" />
         <Route component={Home} exact path="/" />
-        <Route component={Login} exact path="/login" />
-        <Route component={Dashboard} exact path="/dashboard"/>
-        <Route component={Attendance} exact path="/attendance"/>
         <Route component={FacDashboard} exact path="/facdashboard"/>
-        <Route component={TimeTable} exact path="/timetable"></Route>
       </div>
     </Router>
   )

8ae2f4da676eac184433fce8fa44c41070f5ede2
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-14 13:43:09 +0530

    Revert "Minor changes"
    This reverts commit 01e0fcb25eae059d956667c0b2377bb8a8090dde.

--- frontend/src/views/facdashboard.js
+++ frontend/src/views/facdashboard.js
@@ -1,10 +1,14 @@
+import SideNavBar from "../components/SideNavBar";
 import React from "react";
 import './dashboard.css'
 import Dropdown from "../components/dropdown";
+import HorizontalNavBar from '../components/horizontalnavbar';
 
 function FacDashboard() {
     return (
         <div>
+            <HorizontalNavBar />
+            <SideNavBar />
             <Dropdown />
         </div>
     );

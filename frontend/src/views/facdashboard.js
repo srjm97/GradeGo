e4c594bc8b5f5d92df15dcc65f550347d415e484
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-14 13:56:32 +0530

    Minor changes

--- frontend/src/views/facdashboard.js
+++ frontend/src/views/facdashboard.js
@@ -1,14 +1,10 @@
-import SideNavBar from "../components/SideNavBar";
 import React from "react";
 import './dashboard.css'
 import Dropdown from "../components/dropdown";
-import HorizontalNavBar from '../components/horizontalnavbar';
 
 function FacDashboard() {
     return (
         <div>
-            <HorizontalNavBar />
-            <SideNavBar />
             <Dropdown />
         </div>
     );

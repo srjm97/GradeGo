5364cffe6eee61849e92fcd544d810dc7de9d8cd
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-16 11:08:08 +0530

    CodeToNmae schema updated

--- backend/src/models/CodeToName.js
+++ backend/src/models/CodeToName.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const CodeToNameSchema = new mongoose.Schema({
+  //_id is semester number 
+  _id: {
+    type: Number,
+    required: true,
+  },
   courseCode: {
     type: String,
     required: true,

ef1c71039b4d7150099f550b7da19a4c2d5718b5
Neorace22 <siddarthsajeev2002@gmail.com>
2023-05-16 16:16:02 +0530

    Added boiler plate code for getting data from backend

--- frontend/src/pages/TimeTable.js
+++ frontend/src/pages/TimeTable.js
@@ -1,8 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, MenuItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Select, Typography, TextField } from '@mui/material';
 
 
 const Timetable = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('localhost:1337/login'); // Replace '/api/data' with your actual API endpoint
+                if (response.ok) {
+                    const jsonData = await response.json();
+                    setData(jsonData);
+                } else {
+                    console.error('Error fetching data:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+
+    const saveTimetable = async () => {
+        try {
+            const response = await fetch('/api/save-timetable', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    _id: data._id,
+                    batch: data.batch,
+                    days: days.map((day) => ({
+                        day: day,
+                        periods: periods.map((period) => ({
+                            periodNumber: period,
+                            abbreviation: getPeriodAbbreviation(day, period),
+                        })),
+                    })),
+                }),
+            });
+
+            if (response.ok) {
+                console.log('Timetable saved successfully');
+                // Do something on success
+            } else {
+                console.error('Error saving timetable:', response.status);
+            }
+        } catch (error) {
+            console.error('Error saving timetable:', error);
+        }
+    };
+
+
+
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const periods = Array.from(Array(7), (_, index) => index + 1);
 
@@ -89,17 +144,19 @@ const Timetable = () => {
                                     ))}
                                 </TableRow>
                             </TableHead>
+
                             <TableBody>
-                                {timetable.map((entry) => (
-                                    <TableRow key={entry.day}>
-                                        <TableCell style={{ border: "1px solid black" }}>{entry.day}</TableCell>
-                                        {entry.periods.map((period) => (
-                                            <TableCell style={{ border: "1px solid black" }} key={period.periodNumber}>
-                                                {period.value}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))}
+                                {
+                                    timetable.map((entry) => (
+                                        <TableRow key={entry.day}>
+                                            <TableCell style={{ border: "1px solid black" }}>{entry.day}</TableCell>
+                                            {entry.periods.map((period) => (
+                                                <TableCell style={{ border: "1px solid black" }} key={period.periodNumber}>
+                                                    {period.value}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))}
                             </TableBody>
                         </Table>
                     </TableContainer>

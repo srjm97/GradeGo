5c38828fdca31d9e05d734343784e6e7b13e8cc5
RishinR <rishinrnew2020@gmail.com>
2023-05-16 12:34:08 +0530

    restructured timetables schema resolved issues
    added a sample time table which is the currentm time table for s6 b1 cse cet

--- backend/src/models/TimeTable.js
+++ backend/src/models/TimeTable.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoos');
 
 const TimeTableSchema = new mongoose.Schema({
   //semester is the _id in the db
@@ -6,30 +6,231 @@ const TimeTableSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  batch:{
-    type: Number, 
+  batch: {
+    type: Number,
     required: true,
   },
-  day: {
-    type: String,
-    required: true,
-  },
-  periods: [
-    {
-      _id: {
-        type: Number,
-        required: false,
-      },
-      courseCode: {
-        type: String,
-        required: false,
-      },
-      abbreviation: {
-        type: String,
-        required: false,
-      },
+  days: [{
+    day: {
+      type: String,
+      required: true,
     },
-  ],
+    periods: [
+      {
+        _id: {
+          type: Number,
+          required: false,
+        },
+        courseCode: {
+          type: String,
+          required: false,
+        },
+        abbreviation: {
+          type: String,
+          required: false,
+        },
+      },
+    ],
+  }],
 });
 
 module.exports = new mongoose.model('TimeTable', TimeTableSchema);
+
+/*
+db.timetables.insertOne({
+  _id:6,
+  batch:1, 
+  days: [
+    {day:'Monday',
+      periods:[{
+        _id:1,
+        courseCode:'',
+        abbreviation:'CCW'
+      },
+      {
+        _id:2,
+        courseCode:'',
+        abbreviation:'AAD'
+      },
+      {
+        _id:3,
+        courseCode:'',
+        abbreviation:'CD'
+      },
+      {
+        _id:4,
+        courseCode:'',
+        abbreviation:'PP'
+      },
+      {
+        _id:5,
+        courseCode:'',
+        abbreviation:'CGIP'
+      },
+      {
+        _id:6,
+        courseCode:'',
+        abbreviation:'Minor'
+      },
+      {
+        _id:7,
+        courseCode:'',
+        abbreviation:'Minor'
+      }]
+    },
+
+{day:'Tuesday',
+      periods:[{
+        _id:1,
+        courseCode:'',
+        abbreviation:'AAD'
+      },
+      {
+        _id:2,
+        courseCode:'',
+        abbreviation:'IEF'
+      },
+      {
+        _id:3,
+        courseCode:'',
+        abbreviation:'CGIP'
+      },
+      {
+        _id:4,
+        courseCode:'',
+        abbreviation:'CD'
+      },
+      {
+        _id:5,
+        courseCode:'',
+        abbreviation:'PP'
+      },
+      {
+        _id:6,
+        courseCode:'',
+        abbreviation:'Honor'
+      },
+      {
+        _id:7,
+        courseCode:'',
+        abbreviation:'Honor'
+      }]
+    },
+
+{day:'Wednesday',
+      periods:[{
+        _id:1,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini project'
+      },
+      {
+        _id:2,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini Project'
+      },
+      {
+        _id:3,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini project'
+      },
+      {
+        _id:4,
+        courseCode:'',
+        abbreviation:'AAD'
+      },
+      {
+        _id:5,
+        courseCode:'',
+        abbreviation:'CGIP'
+      },
+      {
+        _id:6,
+        courseCode:'',
+        abbreviation:'Free hour'
+      },
+      {
+        _id:7,
+        courseCode:'',
+        abbreviation:'Honour'
+      }]
+    },
+
+{day:'Thursday',
+      periods:[{
+        _id:1,
+        courseCode:'',
+        abbreviation:'PP'
+      },
+      {
+        _id:2,
+        courseCode:'',
+        abbreviation:'IEF'
+      },
+      {
+        _id:3,
+        courseCode:'',
+        abbreviation:'CGIP'
+      },
+      {
+        _id:4,
+        courseCode:'',
+        abbreviation:'AAD'
+      },
+      {
+        _id:5,
+        courseCode:'',
+        abbreviation:'CD'
+      },
+      {
+        _id:6,
+        courseCode:'',
+        abbreviation:'Minor'
+      },
+      {
+        _id:7,
+        courseCode:'',
+        abbreviation:'Minor'
+      }]
+    },
+
+{day:'Friday',
+      periods:[{
+        _id:1,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini Project'
+      },
+      {
+        _id:2,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini Project'
+      },
+      {
+        _id:3,
+        courseCode:'',
+        abbreviation:'Network Lab/Mini Project'
+      },
+      {
+        _id:4,
+        courseCode:'',
+        abbreviation:'Free hour'
+      },
+      {
+        _id:5,
+        courseCode:'',
+        abbreviation:'IEF'
+      },
+      {
+        _id:6,
+        courseCode:'',
+        abbreviation:'CD'
+      },
+      {
+        _id:7,
+        courseCode:'',
+        abbreviation:'Honor'
+      }]
+    }
+  ]
+
+})
+*/
--- backend/src/routes/staffAdvisor.js
+++ backend/src/routes/staffAdvisor.js
@@ -3,26 +3,51 @@ var router = express.Router();
 const TimeTable = require('../models/TimeTable');
 const e = require('express');
 
-router.post('/facdashboard', async (req, res) => {
-  const { day, periods } = req.body;
-  console.log(day, periods);
-  // check if the day is already present if present the update else add new 
-  const present = await TimeTable.findOne({_id:day});
-  console.log(present);
-  if (present) {
-    for (let i = 0; i < periods.length; ++i) {
-      const change = await TimeTable.updateOne({_id:day}, {$set:{[`periods.${i}._id`]:periods[i].index,[`periods.${i}.courseCode`]:periods[i].coursecode,[`periods.${i}.abbreviation`]:periods[i].abbreviation }});
-      // console.log(change);
-    }
-  }
-  else{
-    const initial = await TimeTable.create({_id:day,periods:[]});
-    for (let i = 0; i < periods.length; ++i) {
-      const insert = await TimeTable.updateOne({_id:day}, {$push:{periods:{_id:periods[i].index, courseCode:periods[i].coursecode, abbreviation:periods[i].abbreviation}}})
-      console.log(insert);
-    }
-  }
+//display time table
 
+router.get('/facdashboard/TimeTable', async(req, res)=>{
+  const {semester, batch} = req.body
+});
+
+
+
+
+// to add a new time table or update existing
+router.post('/facdashboard/TimeTable', async (req, res) => {
+  const { semester, batch, days} = req.body;
+  console.log(semester, batch, days);
+  // check if the day is already present if present the update else add new 
+  const present = await TimeTable.findOne({_id:semester, batch:batch});
+  console.log(present);
+  // if (present) {
+  //   for (let i = 0; i < periods.length; ++i) {
+  //     const change = await TimeTable.updateOne({_id:semester, batch:batch}, {$set:{[`periods.${i}._id`]:periods[i].index,[`periods.${i}.courseCode`]:periods[i].coursecode,[`periods.${i}.abbreviation`]:periods[i].abbreviation }});
+  //     // console.log(change);
+  //   }
+  // }
+  // else{
+  //   const initial = await TimeTable.create({_id:semester,batch:batch,periods:[]});
+  //   for (let i = 0; i < periods.length; ++i) {
+  //     const insert = await TimeTable.updateOne({_id:semester, batch:batch}, {$push:{periods:{_id:periods[i].index, courseCode:periods[i].coursecode, abbreviation:periods[i].abbreviation}}})
+  //     console.log(insert);
+  //   }
+  // }
+  if (present) {
+    for (let i = 0; i < days.length; ++i) {
+      for (let j = 0; j < days[i].periods.length; ++j) {
+        const change = await TimeTable.updateOne({_id:semester, batch:batch}, {$set:{[`days.${i}.day`]:days[i].day,[`days.${i}.periods.${j}._id`]:days[i].periods[j].index,[`days.${i}.periods.${j}.courseCode`]:days[i].periods[j].coursecode,[`days.${i}.periods.${j}.abbreviation`]:days[i].periods[j].abbreviation }});
+      }
+    }
+  }
+  else {
+    const initial = await TimeTable.create({_id:semester,batch:batch,days:days});
+    for (let i = 0; i < days.length; ++i) {
+      for (let j = 0; j < days[i].periods.length; ++j) {
+        const insert = await TimeTable.updateOne({_id:semester, batch:batch}, {$push:{days:days}});
+      }
+    }
+    console.log(insert);
+  }
 });
 
 module.exports = router;

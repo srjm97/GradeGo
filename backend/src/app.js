661f3e0d8b1856d834ff4b537f4d7cf5cd8f9d31
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-19 12:15:54 +0530

    jwt token authentication is implemented
    jwt token has to be refreshed and planning to implemented later

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('./connect/mongoose');
-
 const app = express();
 const cors = require('cors');
 //middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//app.use(require('./middlewares/auth'));
 //routes
-
 app.use(require('./routes/login'));
 app.use(require('./routes/index'));
 app.use(require('./routes/staffAdvisor'));
--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,17 +1,25 @@
 var express = require('express');
+require('dotenv').config();
 var router = express.Router();
 const Login = require('../models/Login');
 const Student = require('../models/Student');
 const Faculty = require('../models/Faculty');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const StaffAdvisor = require('../models/StaffAdvisor');
 const CodeToName = require('../models/CodeToName');
+const {authenticateToken} = require('../middlewares/auth');
 
 router.post('/login', async (req, res) => {
   console.log(req.body);
   // check in db if he is a valid user if he is then redirect to dashboard
   const { ktuId, password } = req.body;
   console.log(ktuId, password);
+  //jwt implementation
+  const user = {name:ktuId};
+  const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+  //res.json({accessToken:accessToken});
+
   const pass = await Login.findOne({ _id: ktuId }, { password: 1 });
   console.log(pass);
   if (pass) {
@@ -25,7 +33,7 @@ router.post('/login', async (req, res) => {
       const isStudent = await Student.findOne({ _id: ktuId });
       console.log(isStudent);
       if (isStudent) {
-        return res.json({ status: 'ok', user: 'student' });
+        return res.json({ status: 'ok', user: 'student' ,accessToken:accessToken});
       } else {
         const isFaculty = await Faculty.findOne({_id: ktuId });
         
@@ -34,13 +42,13 @@ router.post('/login', async (req, res) => {
         if (isFaculty) {
           if (isStaffAdvisor) {
             const staffDetails = await StaffAdvisor.findOne({_id: ktuId });
-            console.log(staffDetails);
+            //console.log(staffDetails);
             const courseDetails = await CodeToName.findOne({_id:staffDetails.semesterHandled});
-            console.log(courseDetails);
-            return res.json({ status: 'ok', user: 'faculty', details: staffDetails,course: courseDetails });
+            //console.log(courseDetails);
+            return res.json({ status: 'ok', user: 'faculty', details: staffDetails,course: courseDetails,accessToken:accessToken });
           }
           else {
-            return res.json({ status: 'ok', user: 'faculty' });
+            return res.json({ status: 'ok', user: 'faculty',accessToken:accessToken });
           }
         }
         else{
@@ -55,6 +63,13 @@ router.post('/login', async (req, res) => {
     console.log('invalid username');
     return res.json({ status: 'error', user: 'invalid' });
   }
+});
+
+router.get('/facdashboard',authenticateToken,async(req,res) =>{
+  const logins = await Login.find();
+  console.log(logins);
+  res.json(logins.filter(login=>login._id === req.user.name));
+  
 });
 
 module.exports = router;

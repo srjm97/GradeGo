060c6fa188a524ef4ae96927ee7a1ca3c622abb2
Neorace22 <siddarthsajeev2002@gmail.com>
2023-05-20 12:42:02 +0530

    Fetched timetable data from backend

--- frontend/src/pages/TimeTable.js
+++ frontend/src/pages/TimeTable.js
@@ -8,6 +8,38 @@ const Timetable = () => {
     const { hellodata, setHelloData } = useContext(DataContext);
     const { status, user, details, course } = hellodata;
     console.log(hellodata);
+    const [timetableData, setTimetableData] = useState({});
+    const getTimetableData = async (semester, batch) => {
+        try {
+            
+            const response = await fetch("http://localhost:1337/facdashboard/DisplayTimeTable", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ semester, batch }),
+            });
+
+            if (response.ok) {
+                const timetableDataback = await response.json();
+                setTimetableData(timetableDataback);
+                // Process the timetable data or update state accordingly
+            } else {
+                console.error('Failed to retrieve timetable data:', response.status);
+            }
+        } catch (error) {
+            console.error('Error retrieving timetable data:', error);
+        }
+    };
+
+    useEffect(() => {
+        getTimetableData(details.semesterHandled, details.batchHandled);
+    }, [])
+
+    useEffect(() => {
+        console.log(timetableData);
+    }, [timetableData]);
+
     const handleSubmit = async () => {
         try {
             const response = await fetch('http://localhost:1337/facdashboard/TimeTable', {
@@ -45,7 +77,7 @@ const Timetable = () => {
     const periods = Array.from(Array(7), (_, index) => index + 1);
 
     const [timetable, setTimetable] = useState([]);
-    const [viewTimetable, setViewTimetable] = useState(false);
+    const [viewTimetable, setViewTimetable] = useState(true);
     const [periodInputs, setPeriodInputs] = useState({});
     const [selectedDay, setSelectedDay] = useState('');
 
@@ -62,8 +94,12 @@ const Timetable = () => {
             },
         }));
     };
-
+    const [viewEdit, setViewEdit] = useState(false);
     const handleEdit = () => {
+        setViewEdit(true);
+    }
+
+    const handleSave = () => {
         const updatedTimetable = days.map((day) => ({
             day,
             periods: periods.map((period) => ({
@@ -74,7 +110,8 @@ const Timetable = () => {
 
         setTimetable(updatedTimetable);
         setViewTimetable(true);
-        
+        setViewEdit(false);
+
     };
 
     const getPeriodValue = (day, period) => periodInputs?.[day]?.[period] || '';
@@ -82,38 +119,6 @@ const Timetable = () => {
 
     return (
         <div>
-            <h2>Timetable Editor</h2>
-            <Select value={selectedDay} onChange={handleDayChange} displayEmpty fullWidth>
-                <MenuItem value=''>--Select Day--</MenuItem>
-                {days.map((day) => (
-                    <MenuItem key={day} value={day}>
-                        {day}
-                    </MenuItem>
-                ))}
-            </Select>
-            {selectedDay && (
-                <div>
-                    <Typography variant="h3">{selectedDay}</Typography>
-                    {periods.map((period) => (
-                        <div key={period}>
-                            <Typography variant="subtitle1">Period {period}:</Typography>
-
-                            <Select value={getPeriodValue(selectedDay, period)} onChange={(event) => handlePeriodChange(selectedDay, period, event.target.value)} displayEmpty >
-                                <MenuItem value=''>--Select Subject--</MenuItem>
-
-                                {course.map((c) => (
-                                    <MenuItem key={c.courseAbbreviation} value={c.courseAbbreviation}>
-                                        {c.courseAbbreviation}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </div>
-                    ))}
-                </div>
-            )}
-            <Button style={{ padding: '10px', marginTop: '10px' }} variant="contained" onClick={handleEdit}>
-                Edit
-            </Button>
             {viewTimetable && (
                 <div>
                     <h2>Timetable View</h2>
@@ -145,12 +150,53 @@ const Timetable = () => {
                             </TableBody>
                         </Table>
                     </TableContainer>
+
+                </div>
+
+            )}
+            <Button style={{ padding: '10px', marginTop: '10px' }} variant="contained" onClick={handleEdit}>
+                Edit
+            </Button>
+            {viewEdit && (
+                <div>
+                    <h2>Timetable Editor</h2>
+                    <Select value={selectedDay} onChange={handleDayChange} displayEmpty fullWidth>
+                        <MenuItem value=''>--Select Day--</MenuItem>
+                        {days.map((day) => (
+                            <MenuItem key={day} value={day}>
+                                {day}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                    {selectedDay && (
+                        <div>
+                            <Typography variant="h3">{selectedDay}</Typography>
+                            {periods.map((period) => (
+                                <div key={period}>
+                                    <Typography variant="subtitle1">Period {period}:</Typography>
+
+                                    <Select value={getPeriodValue(selectedDay, period)} onChange={(event) => handlePeriodChange(selectedDay, period, event.target.value)} displayEmpty >
+                                        <MenuItem value=''>--Select Subject--</MenuItem>
+
+                                        {course.map((c) => (
+                                            <MenuItem key={c.courseAbbreviation} value={c.courseAbbreviation}>
+                                                {c.courseAbbreviation}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    <Button style={{ padding: '10px', marginTop: '10px' }} variant="contained" onClick={handleSave}>
+                        Save
+                    </Button>
                     <Button style={{ padding: '10px', marginTop: '10px' }} variant="contained" onClick={handleSubmit}>
                         Submit
                     </Button>
                 </div>
-
             )}
+
         </div>
 
 

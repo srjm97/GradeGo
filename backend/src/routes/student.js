3ca5f93fee82d850dbba6b6502128f3c9e745c99
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-05-24 14:29:10 +0530

    Added more features to admin courses setting

--- backend/src/routes/student.js
+++ backend/src/routes/student.js
@@ -1,15 +1,16 @@
 var express = require('express');
 const InternalMark = require('../models/InternalMark');
 var router = express.Router();
-
+const StudentCourses = require('../models/StudentCourses');
+const Students = require('../models/Student');
 //This route sends attendance details of student for a particular couse
 //Author: Harikrishnan V
-router.get('/attendance/student/', async(req, res) => {
+router.get('/attendance/student/', async (req, res) => {
   //const {ktuId,courseCode} = req.body;
   const ktuId = 'tve20cs000';
   const courseCode = 'CST302';
 
-  const attendanceDetails = await InternalMark.findOne({_id:ktuId,'courseAssessmentTheory.courseCode': courseCode},{'courseAssessmentTheory.courseCode':1,'courseAssessmentTheory.attendance':1});
+  const attendanceDetails = await InternalMark.findOne({ _id: ktuId, 'courseAssessmentTheory.courseCode': courseCode }, { 'courseAssessmentTheory.courseCode': 1, 'courseAssessmentTheory.attendance': 1 });
 
   let presentCount = 0;
   attendanceDetails.courseAssessmentTheory.forEach(course => {
@@ -24,9 +25,38 @@ router.get('/attendance/student/', async(req, res) => {
   //console.log(attendanceDetails);
   const totalAttendanceLength = attendanceDetails.courseAssessmentTheory[0].attendance.length;
   //console.log(attendanceLength);
-  res.json({totalAttendanceLength,presentCount});
+  res.json({ totalAttendanceLength, presentCount });
 
 });
 
+router.post('/student/studentcourses', async (req, res) => {
+  // const { _id } = req.body;
+  const _id = 'tve20cs001';
+
+  try {
+    const student = await Students.findOne({ _id: _id });
+    if (student) {
+      console.log(student.semester);
+      // const courses = await StudentCourses.find({ _id: _id, 'coursesEnrolled.semester': student.semester });
+      // console.log(courses);
+      // return res.json(courses.semesterCourses);
+    } else {
+      // Handle case when student or semester is missing
+      return res.status(404).json({ error: 'Student or semester not found' });
+    }
+  } catch (error) {
+    // Handle any potential errors during database operations
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+
+
+  // const sem = await Students.findOne({ _id: _id });
+  // console.log(sem);
+  // console.log(sem.semester);
+  // const courses = await StudentCourses.find({ _id: _id, 'coursesEnrolled.semester': semester.semester });
+  // console.log(courses);
+  // return res.json(courses.semesterCourses);
+});
 
 module.exports = router;

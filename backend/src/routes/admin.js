c0b3c2ebfe915c1a89ca247683bce88ad3e52edf
RishinR <rishinrnew2020@gmail.com>
2023-05-25 11:25:02 +0530

    faculty course assignment implemented

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Courses = require('../models/Courses');
 const Faculty = require('../models/Faculty');
+const FacultyCourses = require('../models/FacultyCourses');
 
 router.post('/admin/semesterCourses', async (req, res) => {
   const { semester } = req.body;
@@ -11,6 +12,17 @@ router.post('/admin/semesterCourses', async (req, res) => {
   // console.log(faculties);
   // console.log(semesterCourses);
   return res.json({semesterCourses,faculties});
+});
+
+router.post('/admin/facultyCourseAssignment', async (req, res) => {
+  // const {_id, semester, batch, courseCode} = req.body;
+  const _id = 'ktu-f375';
+  const semester = 6;
+  const batch = 1;
+  const courseCode = 'CST302';
+  const addFacultyCourse = await FacultyCourses.updateOne({_id:_id}, {$push:{coursesHandled:{semester:semester, batch:batch, courseCode:courseCode}}});
+  console.log(addFacultyCourse);
+
 });
 
 module.exports = router;

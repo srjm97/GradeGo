32a773f9408a7922f06c83c14a9bdc83fb24397a
RishinR <rishinrnew2020@gmail.com>
2023-05-26 15:17:50 +0530

    resolved errors in student attendance view

--- backend/src/routes/student.js
+++ backend/src/routes/student.js
@@ -6,25 +6,31 @@ const Students = require('../models/Student');
 //This route sends attendance details of student for a particular couse
 //Author: Harikrishnan V
 router.post('/attendance/student/', async (req, res) => {
-  const {ktuId,courseCode} = req.body;
+  const { ktuId, courseCode } = req.body;
   // const ktuId = 'tve20cs000';
   // const courseCode = 'CST302';
-  console.log(ktuId);
-  const attendanceDetails = await InternalMark.findOne({ _id: ktuId, 'courseAssessmentTheory.courseCode': courseCode }, { 'courseAssessmentTheory.courseCode': 1, 'courseAssessmentTheory.attendance': 1 });
-  let presentCount = 0;
-  attendanceDetails.courseAssessmentTheory.forEach(course => {
-    course.attendance.forEach(entry => {
-      if (entry.isPresent === true) {
-        presentCount++;
-      }
+  try {
+    console.log(ktuId);
+    const attendanceDetails = await InternalMark.findOne({ _id: ktuId, 'courseAssessmentTheory.courseCode': courseCode }, { 'courseAssessmentTheory.courseCode': 1, 'courseAssessmentTheory.attendance': 1 });
+    let presentCount = 0;
+    attendanceDetails.courseAssessmentTheory.forEach(course => {
+      course.attendance.forEach(entry => {
+        if (entry.isPresent === true) {
+          presentCount++;
+        }
+      });
     });
-  });
 
-  //console.log(presentCount);
-  //console.log(attendanceDetails);
-  const totalAttendanceLength = attendanceDetails.courseAssessmentTheory[0].attendance.length;
-  //console.log(attendanceLength);
-  res.json({ totalAttendanceLength, presentCount });
+    //console.log(presentCount);
+    //console.log(attendanceDetails);
+    const totalAttendanceLength = attendanceDetails.courseAssessmentTheory[0].attendance.length;
+    //console.log(attendanceLength);
+    res.json({ totalAttendanceLength, presentCount });
+  }
+  catch (error) {
+    res.json({status:'error'});
+    console.log('Error', error);
+  }
 
 });
 
@@ -37,7 +43,7 @@ router.post('/student/studentcourses', async (req, res) => {
     // console.log(student);
     if (student) {
       // console.log(student.currentSemester);
-      const courses = await StudentCourses.findOne({ _id: _id, 'coursesEnrolled.semester':student.currentSemester }, {'coursesEnrolled.semesterCourses':1});
+      const courses = await StudentCourses.findOne({ _id: _id, 'coursesEnrolled.semester': student.currentSemester }, { 'coursesEnrolled.semesterCourses': 1 });
       console.log(courses);
       return res.json(courses.coursesEnrolled);
     } else {

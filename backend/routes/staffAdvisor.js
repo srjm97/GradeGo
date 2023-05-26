231b3f2a57a0f100750e1a76aedb788e08cb1655
RishinR <rishinrnew2020@gmail.com>
2023-05-26 10:46:40 +0530

    removed unwanted files

--- backend/routes/staffAdvisor.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const TimeTable = require("../models/TimeTable");
-
-router.post("/faculty", async (req, res) => {
-  const { _id, periods } = req.body;
-});
-
-module.exports = router;

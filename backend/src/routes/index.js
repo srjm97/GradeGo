ee15df1fd2211fee7a524705c31572a0577586ca
Hari2k02 <harivellikkunnam@gmail.com>
2023-05-29 11:27:30 +0530

    render method changed to send

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.sendStatus('index', { title: 'Express' });
+router.get('/', (req, res)=> {
+  res.send('index', { title: 'Express' });
 });
 
 module.exports = router;
